995e0bc644a0c3ba3f7b1f44bb11ff49115e40a7
Victor Costa <joaovictorcope@gmail.com>
2026-03-15 15:19:26 -0300

    githubpages deploy versão 0.0

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Timeline from './components/Timeline';
+import './index.css';
+
+function App() {
+    return (
+        <div>
+            <Timeline />
+        </div>
+    );
+}
+
+export default App;
--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/linha_do_tempo_clube_de_livro/'
 })
